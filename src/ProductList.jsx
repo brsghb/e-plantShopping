3aac6dbc08687a7647f0ae6b1d53a75a2ece2d29
Barış Özdağ <gh@ozdag.net>
2026-02-05 23:57:11 +0300

    update total quantity calculation

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -271,6 +271,10 @@ function ProductList({ onHomeClick }) {
         }));
     };
 
+    const calculateTotalQuantity = () => {
+        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -297,7 +301,7 @@ function ProductList({ onHomeClick }) {
                                     <circle cx="184" cy="216" r="12"></circle>
                                     <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
                                 </svg>
-                                {totalQuantity > 0 && <span className="cart_quantity_count">{totalQuantity}</span>}
+                                {<span className="cart_quantity_count">{calculateTotalQuantity()}</span>}
                             </h1>
                         </a>
                     </div>
